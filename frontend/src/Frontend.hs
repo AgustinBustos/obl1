3c29a2764d7e28e7c55296e76b09ad343c84221c
Agustin Bustos Barton <agustinbustosbarton@gmail.com>
2025-08-31 18:26:39 -0300

    too little storage

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -10,6 +10,7 @@ import Control.Monad
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import Language.Javascript.JSaddle (liftJSM, js, js1, jsg)
+import Language.Javascript.JSaddle.Evaluate (eval)
 
 import Obelisk.Frontend
 import Obelisk.Configs
@@ -25,6 +26,10 @@ import Common.Route
 -- This runs in a monad that can be run on the client or the server.
 -- To run code in a pure client or pure server context, use one of the
 -- `prerender` functions.
+
+
+
+
 frontend :: Frontend (R FrontendRoute)
 frontend = Frontend
   { _frontend_head = do
@@ -32,27 +37,30 @@ frontend = Frontend
       elAttr "script" ("type" =: "application/javascript" <> "src" =: $(static "lib.js")) blank
       elAttr "link" ("href" =: $(static "main.css") <> "type" =: "text/css" <> "rel" =: "stylesheet") blank
   , _frontend_body = do
-      el "h1" $ text "Welcome agus!"
-      -- el "p" $ text $ T.pack commonStuff
+      el "div" $ do
+      	el "p" $ text "write something"
+	elAttr "audio" ("controls" =: "" <> "src" =: $(static "sounder.mp3")) blank
+--	welcomeComp
+	t <- inputElement def
+	text " "
+	el "ol" $ do
+	 el "li" $ text "something"
+	 el "li" $ dynText $ _inputElement_value t
+	text " "
+	prerender_ blank $ liftJSM $ void
+	         $ jsg ("window" :: T.Text)
+	         ^. js ("skeleton_lib" :: T.Text)
+	         ^. js1 ("log" :: T.Text) ("Hello, World!" :: T.Text)
+		 ^. js1 ("new Audio('alarm.mp3').play();" :: T.Text)
 
-      -- -- `prerender` and `prerender_` let you choose a widget to run on the server
-      -- -- during prerendering and a different widget to run on the client with
-      -- -- JavaScript. The following will generate a `blank` widget on the server and
-      -- -- print "Hello, World!" on the client.
-      -- prerender_ blank $ liftJSM $ void
-      --   $ jsg ("window" :: T.Text)
-      --   ^. js ("skeleton_lib" :: T.Text)
-      --   ^. js1 ("log" :: T.Text) ("Hello, World!" :: T.Text)
-
-      -- elAttr "img" ("src" =: $(static "obelisk.jpg")) blank
-      -- el "div" $ do
-      --   let
-      --     cfg = "common/example"
-      --     path = "config/" <> cfg
-      --   getConfig cfg >>= \case
-      --     Nothing -> text $ "No config file found in " <> path
-      --     Just bytes -> case T.decodeUtf8' bytes of
-      --       Left ue -> text $ "Couldn't decode " <> path <> " : " <> T.pack (show ue)
-      --       Right s -> text s
+	
+	--(btn, _) <- el' "button" $ text "Start alarm"
+	--let clickEv = domEvent Click btn
+	-- Start a timer 5 seconds after click
+	--tickEv <- delay 5 clickEv
+	--performEvent_ $ ffor tickEv $ \_ -> do
+	 --liftJSM $ void $ eval ("new Audio('alarm.mp3').play();" :: T.Text)
       return ()
   }
+
+--welcomeComp = el "h1" $ text "Welcome agus!"
